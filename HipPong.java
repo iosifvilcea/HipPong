a7c28bb64ec787a03e8ac403c59a4efaf67a2202
emddeleon4/em <emddeleon4@gmail.com/em>
2014-11-05 13:49:40 -0500

    Added plans.jpg(whiteboard picture of plans) and HipPong.java with outline of what to do in it.

---
+++ HipPong.java
@@ -0,0 +1,31 @@
+// ? things left to do
+
+//*****************************************************************************
+//Class:
+//	HipPong
+//
+//	Operations:
+//		This class is the main class. It uses the Player, Computer,
+//		Ball, and BlackHole classes to run the game. It's main
+//		is function painting the field, ball, blackhole, computers
+//		and players constantly. It also takes in input from the
+//		and translates it the the movement of the paddles. Lastely,
+//		it runs all the updates for the movement of all the objects.
+//*****************************************************************************
+public class HipPong{
+	public void main(String[] args){
+		//Draw main menu where which players are human players,
+		//	computer players or walls are choosen as drawn
+		//	on the top of the included image in this folder titled
+		//	plans.jpg. Should also have the ability to select
+		//	keyboard inputs for each player (any keyboard inputs
+		//	entered for computers or walls will be ignored when
+		//	setting everything up).
+
+		//Setup the game according to the choices made in the main menu.
+		//	Declare all the classes with the correct passed in
+		//	parameters.
+
+		//	
+	}
+}
