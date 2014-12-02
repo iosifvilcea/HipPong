afb13b7f1d8fad9c117a6da73b6dbbafd693c744
Vilcea <iosifvilcea@gmail.com>
2014-12-02 02:40:45 -0500

    Check README! Added P2 Paddle.

--- Game.java
+++ Game.java
@@ -34,6 +34,8 @@ public class Game extends JPanel implements ActionListener, KeyListener{
     // *********************
     // Player Attributes
     // *********************
+
+    //LEFT
     private int p1x = 25;
     private int p1y = 250;
     private int p1Width = 10;
@@ -42,7 +44,18 @@ public class Game extends JPanel implements ActionListener, KeyListener{
 
     private boolean p1LeftPress = false;
     private boolean p1RightPress = false;
-   
+
+    //RIGHT
+    private int p2x = 465;
+    private int p2y = 250;
+    private int p2Width = 10;
+    private int p2Height = 50;
+    private int p2Speed = 5;
+
+    private boolean p2LeftPress = false;
+    private boolean p2RightPress = false;
+
+
     // ********************
     //  Constructor
     // ********************
@@ -67,7 +80,7 @@ public class Game extends JPanel implements ActionListener, KeyListener{
         g.setColor(Color.WHITE);                    //Set Component Colors
         g.fillOval(ballX,ballY,diameter,diameter);  //Create Ball
         g.fillRect(p1x, p1y, p1Width, p1Height);    //Create P1 Paddle
-        
+        g.fillRect(p2x, p2y, p2Width, p2Height);    //Create P2 Paddle 
     }
 
     // ***********************************
@@ -82,18 +95,41 @@ public class Game extends JPanel implements ActionListener, KeyListener{
     // Handles KeyEvents
     // ***********************************
     public void keyPressed(KeyEvent e){
-        if( "Q".equals( e.getKeyText( e.getKeyCode() ) ) ){
+
+        //LEFT PLAYER
+        if( e.getKeyCode() == KeyEvent.VK_Q )
             p1LeftPress = true;
-        }
-        if( "A".equals( e.getKeyText( e.getKeyCode() ) ) )
+        
+        if(e.getKeyCode() == KeyEvent.VK_A )
             p1RightPress = true;
-    }
-    public void keyReleased(KeyEvent e){
-	    p1LeftPress = false;
-	    p1RightPress = false;
+
+        //RIGHT PLAYER
+        if( e.getKeyCode() == KeyEvent.VK_M )
+            p2LeftPress = true;
+        if( e.getKeyCode() == KeyEvent.VK_COMMA )
+            p2RightPress = true;
+
+        //TOP PLAYER
+        
+        //BOTTOM PLAYER
     }
 
-    public void keyTyped(KeyEvent e){}
+    public void keyReleased(KeyEvent e){
+        //LEFT PLAYER
+        if( e.getKeyCode() == KeyEvent.VK_Q ) 
+            p1LeftPress = false;
+        if( e.getKeyCode() == KeyEvent.VK_A ) 
+            p1RightPress = false;
+
+        //RIGHT PLAYER
+        if( e.getKeyCode() == KeyEvent.VK_M )
+            p2LeftPress = false;
+        if( e.getKeyCode() == KeyEvent.VK_COMMA )
+            p2RightPress = false;
+
+    }
+
+    public void keyTyped(KeyEvent e){ /* Not needed yet. */ }
 
     // ***********************************
     //  Run()
@@ -102,32 +138,62 @@ public class Game extends JPanel implements ActionListener, KeyListener{
     // ***********************************
     public void run(){
 
-        if(p1LeftPress){
-            System.out.print("Left Pressed.\n");
-            if(p1y - p1Speed > 0) 
+        // Player1/LeftSide
+        if(p1LeftPress)
+            if( (p1y - p1Speed) > 0) 
                 p1y -= p1Speed;
-        }
 
-        if(p1RightPress){
-            System.out.print("Right Pressed.\n"); 
-            if(p1y + p1Speed + p1Height < getHeight())
+        if(p1RightPress)
+            if( (p1y + p1Speed + p1Height) < getHeight())
                 p1y += p1Speed;
-        }
+
+        //Player2/RightSide
+        if(p2LeftPress)
+            if( (p2y - p2Speed) > 0) 
+                p2y -= p1Speed;
+
+        if(p2RightPress)
+            if( (p2y + p2Speed + p2Height) < getHeight())
+                p2y += p2Speed;
 
 
         //Check for boundaries.
         //Left Side
-        if(ballX < (p1x + p1Width)){                //If ball Passes Paddle Position
-            if (ballY+ballDY > (p1y + p1Height) || 
-               (ballY+ballDY + diameter) < p1y)     //If ball Misses the paddle
-                System.out.println("Miss.");            //Game Blouses.
-            else                                    //Else, were good, it hit.
+        if(ballX < (p1x + p1Width)){                //If ball passes Left Paddle Position
+            if (ballY+ballDY > (p1y + p1Height) || (ballY+ballDY + diameter) < p1y)     //If ball Misses the paddle
+            {    
+                //Announce Winner, Score Points
+                System.out.println("P1 Loses.");            //Game Blouses.
+               
+                //Reset Paddles
+                p1x = 25;
+                p2x = 465;
+
+                //Reset Ball Position
+                ballX = 250;
+                ballY = 250;
+
+            }else                                    //Else, were good, it hit.
                 ballDX = Math.abs(ballDX);
         }
 
         //Right Side
-        if(ballX > getWidth() - diameter/2){
-            ballDX = -Math.abs(ballDX);
+        if(ballX > (p2x + p2Width)){                 //If ball Right Passes Paddle Position
+            if (ballY+ballDY > (p2y + p2Height) || (ballY+ballDY + diameter) < p2y)
+            {
+                //Announce Winner, Score Points
+                System.out.println("P2 Loses.");            //Game Blouses.
+               
+                //Reset Paddles
+                p1x = 25;
+                p2x = 465;
+
+                //Reset Ball Position
+                ballX = 250;
+                ballY = 250;
+
+            }else
+                ballDX = -Math.abs(ballDX);
         }
 
         //Top
--- README.md
+++ README.md
@@ -2,3 +2,17 @@ HipPong
 =======
 
 Original Pong but way cooler.
+
+TODO:
+    See Through Menu
+    Integrate Menu
+    Fix 2nd Player Paddle
+    Add P3, P4.
+    Add BlackHole
+    Add Point System
+    Fixed Screen Size
+   
+NOTES:
+    Ball doesn't detect 2nd Player Paddle in the right location.
+    Will be fixed once we have a fixed size for the screen.
+    1200x900 is way too big. 
